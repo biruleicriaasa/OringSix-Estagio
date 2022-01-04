00d62aabc14d8bb252d50aafd5d1cc08e933cbcc
biruleicriaasa <biruleicriaasa@gmail.com>
2022-01-04 18:50:32 -0300

    refactore: added coment

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
 //DOM Document Object Model para acessar as propriedades do documento
 
-
 // para abrir e fechar o menu da pagina web quando clicar nos icons do menu
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
-for(const element of toggle){ // para ("tipo_variavel" "nomeVariavelQualquer" of "Referencia_De_UmaVariavel_Exisnte_Do_MesmoTipo")
-  element.addEventListener ('click', // o EVENTO DESEJADO
-  function(){ // declarando função sem nome
-    nav.classList.toggle('show');
-  });
+for (const element of toggle) {
+  // para ("tipo_variavel" "nomeVariavelQualquer" of "Referencia_De_UmaVariavel_Exisnte_Do_MesmoTipo")
+  element.addEventListener(
+    'click', // o EVENTO DESEJADO
+    function () {
+      // declarando função sem nome
+      nav.classList.toggle('show')
+    }
+  )
 }
 
 // quando clicar em qualquer item linkado do menu, feche o menu
-const linksMenu = document.querySelectorAll('#header nav .menu ul li a');
+const linksMenu = document.querySelectorAll('#header nav .menu ul li a')
 
-for(const links of linksMenu){
-  links.addEventListener('click',
-  function(){
+for (const links of linksMenu) {
+  links.addEventListener('click', function () {
     nav.classList.remove('show')
-  });
+    // teste aqui
+  })
 }
 
 //mudar o header da página quando der scroll
-const header = document.querySelector('#header');
-const alturadoHeader = header.offsetHeight;
+const header = document.querySelector('#header')
+const alturadoHeader = header.offsetHeight
 
-window.addEventListener('scroll', 
-function() {
-  if(window.scrollY >= alturadoHeader){
-    header.classList.add('scroll');
-  }else{
-    header.classList.remove('scroll');
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= alturadoHeader) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
   }
-}) 
-
-
+})
